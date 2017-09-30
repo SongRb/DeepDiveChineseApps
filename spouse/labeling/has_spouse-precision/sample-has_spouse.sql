c4fb5b77ef67f17c56d8e278299341fb91a2a04b
tang <tangsongkai2287@gmail.com>
2017-09-30 10:55:02 +0800

    Add a new app for locatio relationship

---
+++ spouse/labeling/has_spouse-precision/sample-has_spouse.sql
@@ -0,0 +1,29 @@
+SELECT hsi.p1_id
+     , hsi.p2_id
+     , s.doc_id
+     , s.sentence_index
+     , hsi.label
+     , hsi.expectation
+     , s.tokens
+     , pm1.mention_text AS p1_text
+     , pm1.begin_index  AS p1_start
+     , pm1.end_index    AS p1_end
+     , pm2.mention_text AS p2_text
+     , pm2.begin_index  AS p2_start
+     , pm2.end_index    AS p2_end
+
+  FROM has_spouse_label_inference hsi
+     , person_mention             pm1
+     , person_mention             pm2
+     , sentences                  s
+
+ WHERE hsi.p1_id          = pm1.mention_id
+   AND pm1.doc_id         = s.doc_id
+   AND pm1.sentence_index = s.sentence_index
+   AND hsi.p2_id          = pm2.mention_id
+   AND pm2.doc_id         = s.doc_id
+   AND pm2.sentence_index = s.sentence_index
+   AND       expectation >= 0.9
+
+ ORDER BY random()
+ LIMIT 100
